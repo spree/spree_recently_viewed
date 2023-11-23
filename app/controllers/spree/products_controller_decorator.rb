7881be443e9e463e86feb6f3a4c25777f0d5d448
rahul-bash <rahul-dev@bluebash.co>
2023-11-23 11:04:58 +0530

    user specific recently viewed

--- app/controllers/spree/products_controller_decorator.rb
+++ app/controllers/spree/products_controller_decorator.rb
@@ -4,6 +4,7 @@ module Spree::ProductsControllerDecorator
     base.helper_method [:cached_recently_viewed_products, :cached_recently_viewed_products_ids]
     base.before_action :set_current_order, except: :recently_viewed
     base.after_action :save_recently_viewed, only: :recently_viewed
+    base.before_action :clear_recently_viewed_cookie_on_user_change
   end
 
   def recently_viewed
@@ -22,6 +23,21 @@ module Spree::ProductsControllerDecorator
     rvp_max_count = Spree::RecentlyViewed::Config.preferred_recently_viewed_products_max_count
     rvp.delete_at(0) if rvp.size > rvp_max_count.to_i
     cookies['recently_viewed_products'] = rvp.join(', ')
+  end
+
+  def clear_recently_viewed_cookie_on_user_change
+    return if spree_current_user.nil? && cookies['previous_user_id'] = 0
+
+    return if cookies['previous_user_id'] == spree_current_user.id.to_s
+
+    cookies.delete('previous_user_id')
+    clear_recently_viewed_cookie
+    cookies['previous_user_id'] = spree_current_user.id
+  end
+
+  def clear_recently_viewed_cookie
+    rvp_cookie_name = "recently_viewed_products"
+    cookies.delete(rvp_cookie_name)
   end
 end
 
